e90afc01f0cfc81078cba3ebee0c85ae17b7b021
Souren Papazian <souren@papazian.info>
2015-06-10 13:16:39 -0700

    made crawler wrapper more abstracted

--- src/crawl.js
+++ src/crawl.js
@@ -1,17 +1,16 @@
 var _ = require('lodash');
 var Crawler = require('./lib/crawler').Crawler;
 
-function newCrawler(maxRequests, logger) {
-  return new Crawler(maxRequests, logger);
+function initCrawler(maxRequests, logger) {
+  this.crawler = new Crawler(maxRequests, logger);
 }
 
-Crawler.prototype.crawl = function(entryIp) {
-  var self = this;
-  return new Promise(function(resolve, reject){
-    self.once('done', function(response) {
+initCrawler.prototype.crawl = function(entryIp) {
+  return new Promise(function(resolve, reject) {
+    this.crawler.once('done', function(response) {
       resolve(response)
     }).enter(entryIp)
   })
 }
 
-exports.Crawler = newCrawler;
+exports.Crawler = initCrawler;
