49c4de16711fa0204f040d6c41efe8b478ca1d82
Hayk Tepanyan <tehayk@stanford.edu>
2015-08-06 15:37:28 -0700

    Added pool to sequelize. Should help to avoid Frequent Database errors.

--- src/lib/database.js
+++ src/lib/database.js
@@ -4,9 +4,20 @@ var sql;
 var sqlInited;
 
 module.exports = {
-   initSql: function(dbUrl, log) {
+  initSql: function(dbUrl, log) {
     if (!sqlInited) {
-      sql = new Sequelize(dbUrl, {logging: log, dialectOptions: {ssl: true}});
+      sql = new Sequelize(dbUrl,
+      {
+        logging: log,
+        dialectOptions: {
+          ssl: true
+        },
+        pool: {
+          maxConnections: 10,
+          minConnections: 0,
+          maxIdleTime: 10000
+        }
+      });
       sqlInited = true;
     }
     return sql;
