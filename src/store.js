a0f1c6e681775809264f2166412198cfb8af23f5
Hayk Tepanyan <tehayk@stanford.edu>
2015-08-10 15:28:00 -0700

    limbo state bug fixed.

--- src/store.js
+++ src/store.js
@@ -4,35 +4,26 @@ var modelsFactory = require('./lib/models.js');
 var Promise = require('bluebird');
 
 function saveDB(crawlJson, dbUrl, logsql) {
-  return new Promise(function(resolve, reject) {
-    var log = logsql ? console.log : false;
-    var sql = DB.initSql(dbUrl, log);
-    var models = modelsFactory(sql);
-
-    sql
-    .sync({force: false})
-    .then(function() {
-      models.Crawl.create({start_at: crawlJson.start,
-                                  end_at: crawlJson.end,
-                                  entry_ipp: crawlJson.entry,
-                                  data: crawlJson.data,
-                                  exceptions: crawlJson.errors});
-    })
-    .then(resolve)
-    .catch(function(err){
-      console.error(err);    // this is a serious error
-      process.exit(1);
-    });
-  });
+  var log = logsql ? console.log : false;
+  var sql = DB.initSql(dbUrl, log);
+  var models = modelsFactory(sql);
+  return models.Crawl.create({start_at: crawlJson.start,
+                                end_at: crawlJson.end,
+                                entry_ipp: crawlJson.entry,
+                                data: crawlJson.data,
+                                exceptions: crawlJson.errors});
 }
 
 module.exports = function(crawl, dbUrl, logsql) {
   return new Promise(function(resolve, reject) {
     saveDB(crawl, dbUrl, logsql)
     .then(function() {
-      console.log("Stored data crawled at: " + crawl.start);
+      console.log('Stored data crawled at: ' + crawl.start);
       resolve(crawl);
     })
-    .catch(reject);
+    .catch(function(err) {
+      console.error(err);    // this is a serious error
+      process.exit(1);
+    });
   });
 };
