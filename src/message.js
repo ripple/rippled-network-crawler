947ef68a9c1f4cb1fd87a2cdc32975e93953fba9
Souren Papazian <souren@papazian.info>
2015-08-17 16:37:59 -0700

    added missing message.js file

---
+++ src/message.js
@@ -0,0 +1,23 @@
+var AWS = require('aws-sdk');
+var moment = require('moment');
+AWS.config.region = 'us-west-2';
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+module.exports = function(row, queueUrl) {
+  return new Promise(function(resolve, reject) {
+    var params = {
+      MessageBody: row.id, /* required */
+      QueueUrl: queueUrl, /* required */
+      DelaySeconds: 0
+    };
+    sqs.sendMessage(params, function(error, data) {
+      if (error) {
+        reject(error);
+      } else {
+        console.log('Queued crawl %d (%s) \t at %s \t to %s',
+                row.id, moment(row.start_at).format(),  moment().format(), queueUrl);
+        resolve(data);
+      }
+    });
+  });
+};
